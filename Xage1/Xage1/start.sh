7cd4e1fce296111ba1ba13032710eaad471d8079
jude <jude-simon.agohoundje@epitech.eu>
2023-09-01 15:15:26 +0100

    ajout url

--- Xage1/Xage1/start.sh
+++ Xage1/Xage1/start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-pip install --upgrade typing-extensions
-
-# Activation de l'environnement virtuel (si applicable)
-source django-apps/env/bin/activate
-
-# Lancement de Gunicorn avec votre application Django
-gunicorn xage.wsgi:application --bind 0.0.0.0:$PORT --workers 4
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver
