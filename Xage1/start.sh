5f56e1900a82d3c86d835d6d682cabf6e4a53d66
jude <jude-simon.agohoundje@epitech.eu>
2023-09-05 16:06:00 +0100

    ajoute

--- Xage1/start.sh
+++ Xage1/start.sh
@@ -2,4 +2,4 @@
 python manage.py makemigrations
 python manage.py migrate
 
-gunicorn xage.wsgi:application --bind 0.0.0.0:$PORT --workers 4
+gunicorn xage1.wsgi:application --bind 0.0.0.0:$PORT --workers 4
